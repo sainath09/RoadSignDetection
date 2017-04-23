de00956af04a3cefee4f4a119be107cd55d289c3
KPS <sainath@ufl.edu>
2017-04-23 12:26:54 -0400

    working on testing

--- hogex.py
+++ hogex.py
@@ -5,7 +5,8 @@ import proto.createHogMatrix as hm
 
 
 if __name__=="__main__":
-  a=[0,37]
+  a=range(0,43)
+  print a
   twoDHogVals = hm.createHMatrix(a)
   twoDHogVals = twoDHogVals.astype(np.float32)
   cls.train_test_svm(twoDHogVals, twoDHogVals)
--- proto/createHogMatrix.py
+++ proto/createHogMatrix.py
@@ -1,7 +1,7 @@
 import numpy as np
 import csv
 
-def createHMatrix(i,j):
+def createHMatrix(a):
   '''input : two class indices i and j 
    output :  a 2 d array with hog va;ues of images stacked up one over the other for classes i and j 
   '''
@@ -10,11 +10,7 @@ def createHMatrix(i,j):
   gtFilePath = "final_training/Final_Training/Images/"
   imageNumber = 0
 # find the number of images
-  for classNumber in range(0,2):
-    if classNumber is 0:
-      classIndex=i
-    else :
-      classIndex=j	
+  for classIndex in a:
     gtClassFileName=gtFilePath+format(classIndex,'05d')+"/"+'GT-'+ format(classIndex, '05d') + '.csv'
     gtFile = open(gtClassFileName,'r')
     gtReader = csv.reader(gtFile, delimiter=';')
@@ -28,11 +24,7 @@ def createHMatrix(i,j):
   twoDHogVals = np.zeros((imageNumber,1569))
   index = 0
 #for writing the hog values into a 2 D array with each row has hog vaklues of each image	
-  for classNumber in range (0,2): 
-    if classNumber is 0:
-      classIndex = i
-    else :
-      classIndex = j	
+  for classIndex in a:
     gtClassFileName=gtFilePath+format(classIndex,'05d')+"/"+'GT-'+ format(classIndex, '05d') + '.csv'
     gtFile = open(gtClassFileName,'r')
     gtReader = csv.reader(gtFile, delimiter=';')
@@ -41,7 +33,7 @@ def createHMatrix(i,j):
       cindex = row[0].split("_")
       cIndex=int(cindex[0])
     gtFile.close()	
-    print cIndex	
+    print classIndex, " " ,cIndex	
     for c in range(0,cIndex+1):
       for k in range(0,30):
         fileName = HOGfilepath+format(classIndex,'05d')+"/"+format(c,'05d')+"_"+format(k,'05d')+".txt"
@@ -54,6 +46,6 @@ def createHMatrix(i,j):
         index = index+1	
         file.close()		
     #print twoDHogVals
-  np.savetxt('proto/data/hog.csv', twoDHogVals, delimiter=', ')
+  #np.savetxt('proto/data/hog.csv', twoDHogVals, delimiter=', ')
   return twoDHogVals
 # end of function createMatrix
