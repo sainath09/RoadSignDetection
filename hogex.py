6fbed5ddb08526789e2e64a1a845ab3b96aa8801
KPS <sainath@ufl.edu>
2017-04-23 16:04:09 -0400

    model created

--- hogex.py
+++ hogex.py
@@ -7,6 +7,27 @@ import proto.createHogMatrix as hm
 if __name__=="__main__":
   a=range(0,43)
   print a
+  twoDTrainHogVals = np.zeros((1,1569))
+  twoDTestHogVals = np.zeros((1,1569))
   twoDHogVals = hm.createHMatrix(a)
+  i=0	
   twoDHogVals = twoDHogVals.astype(np.float32)
-  cls.train_test_svm(twoDHogVals, twoDHogVals)
+  twoDTrainHogVals = twoDTrainHogVals.astype(np.float32)
+  twoDTestHogVals = twoDTestHogVals.astype(np.float32)
+  list_2r =[]
+  list_1r =[]
+  i2 = 0
+  i1 = 2
+  while(i1<twoDHogVals.shape[0]):
+    samp_00 = twoDHogVals[i2, :]
+    samp_01 = twoDHogVals[i2+1, :]
+    list_2r.append(samp_00)
+    list_2r.append(samp_01)
+    list_1r.append(twoDHogVals[i1,:])
+    i2=i2+3
+    i1=i1+3
+  print i1
+  mat_2r = np.array(list_2r)
+  mat_1r = np.array(list_1r)
+  print mat_2r.shape, mat_1r.shape
+  cls.train_test_svm(mat_2r, mat_1r)
